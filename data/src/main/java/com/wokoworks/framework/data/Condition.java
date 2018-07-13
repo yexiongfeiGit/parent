05be13491faf220090ed275d2c33c5cbdbe2e228
0x0001 <78712900@qq.com>
2018-07-13 16:48:49 +0800

    添加javaDoc注释

--- data/src/main/java/com/wokoworks/framework/data/Condition.java
+++ data/src/main/java/com/wokoworks/framework/data/Condition.java
@@ -5,12 +5,34 @@ package com.wokoworks.framework.data;
  */
 public interface Condition {
 
+    /**
+     * 获取sql语句
+     *
+     * @return
+     */
     String getSql();
 
+    /**
+     * 获取参数列表
+     *
+     * @return
+     */
     Object[] getArgs();
 
+    /**
+     * 通过另外一个条件组合成一个or条件
+     *
+     * @param condition 子条件
+     * @return 新的组合条件
+     */
     Condition or(Condition condition);
 
+    /**
+     * 通过另外一个条件组合为一个and条件
+     *
+     * @param condition 子条件
+     * @return 新的组合条件
+     */
     Condition and(Condition condition);
 
 }
