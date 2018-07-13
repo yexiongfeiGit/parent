ca9ab82d251e6548249d6fce9be6c13d60a55066
0x0001 <78712900@qq.com>
2018-07-13 16:50:44 +0800

    添加JavaDoc注释

--- data/src/main/java/com/wokoworks/framework/data/impl/sqlbuilder/SqlExecuteProvider.java
+++ data/src/main/java/com/wokoworks/framework/data/impl/sqlbuilder/SqlExecuteProvider.java
@@ -6,7 +6,17 @@ import com.wokoworks.framework.data.impl.JdbcTemplateUtils;
  * @author 0x0001
  */
 public interface SqlExecuteProvider<T> {
+    /**
+     * 获取对应表名
+     *
+     * @return 要操作的表的名称
+     */
     String getTableName();
 
+    /**
+     * 获取jdbc template操作工具对象
+     *
+     * @return 对应的jdbc操作工具对象
+     */
     JdbcTemplateUtils<T> getJdbcTemplateUtils();
 }
