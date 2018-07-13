bd3d5eba65120a2249ce635299c7074fa474ebd1
0x0001 <78712900@qq.com>
2018-07-13 16:49:31 +0800

    添加javaDoc注释

--- data/src/main/java/com/wokoworks/framework/data/Condition.java
+++ data/src/main/java/com/wokoworks/framework/data/Condition.java
@@ -8,14 +8,14 @@ public interface Condition {
     /**
      * 获取sql语句
      *
-     * @return
+     * @return 对应条件生成的sql片段
      */
     String getSql();
 
     /**
      * 获取参数列表
      *
-     * @return
+     * @return 对应条件需要的参数列表
      */
     Object[] getArgs();
 
