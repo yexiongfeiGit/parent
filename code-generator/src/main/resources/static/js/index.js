ab36ae33f5f67efc341b754c4797a56ae506f59f
0x0001 <78712900@qq.com>
2018-07-17 07:55:49 +0800

    模板调整

--- code-generator/src/main/resources/static/js/index.js
+++ code-generator/src/main/resources/static/js/index.js
@@ -17,11 +17,11 @@
 
     Vue.component('list-panel', {
         template: document.getElementById('list-panel').innerText,
-        props: ['name', 'list', 'has_add_button', 'single_choose'],
+        props: ['title', 'list', 'has_add_button', 'single_choose'],
         methods: {
             item_click: function(item) {
                 if (item.active && this.single_choose) {
-                    if(this.current_item) {
+                    if(this.current_item && this.current_item != item) {
                         this.current_item.active = false;
                     }
                     this.current_item = item;
@@ -37,12 +37,15 @@
         }
     });
 
+    // -- app ...
+
     var database = [];
 
     for (var i = 0; i < 10; i++) {
         database.push({
             name: i,
             active: false,
+            has_info: true,
         });
     }
 
@@ -62,7 +65,7 @@
                 console.log('conn info click', item);
             },
             conn_add_button_click: function() {
-                console.log('conn add click', item);
+                console.log('conn add click');
             }
         }
     });
