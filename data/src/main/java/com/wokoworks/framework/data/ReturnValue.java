a41f0a38fc09d1843755a3e2537000a41097f544
tzw <tzw@pundix.com>
2018-07-19 20:06:58 +0800

    returnvalue

---
+++ data/src/main/java/com/wokoworks/framework/data/ReturnValue.java
@@ -0,0 +1,46 @@
+package com.wokoworks.framework.data;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.NoTransactionException;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
+
+import java.io.Serializable;
+
+/**
+ * @author 0x0001
+ */
+@Data
+@Slf4j
+public class ReturnValue<T, E extends Enum> implements Serializable {
+
+    private T data;
+    private E error;
+
+    private ReturnValue() {
+        //
+    }
+
+    public boolean hasError() {
+        return error != null;
+    }
+
+    public static <T, E extends Enum> ReturnValue<T, E> withOk(T data) {
+        final ReturnValue<T, E> value = new ReturnValue<>();
+        value.setData(data);
+        return value;
+    }
+
+    public static <T, E extends Enum> ReturnValue<T, E> withError(E error) {
+        // 回滚事务
+        try {
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+        } catch (NoTransactionException e) {
+            log.debug(e.getMessage(), e);
+            // quiet
+        }
+        final ReturnValue<T, E> value = new ReturnValue<>();
+        value.setError(error);
+        return value;
+    }
+}
