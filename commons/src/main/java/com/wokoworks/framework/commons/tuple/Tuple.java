e26691a1d4d2f284eb75a7a9b851a608b211fde5
0x0001 <78712900@qq.com>
2018-11-23 10:46:21 +0800

    基本数据类型元组,减少装/拆箱操作

--- commons/src/main/java/com/wokoworks/framework/commons/tuple/Tuple.java
+++ commons/src/main/java/com/wokoworks/framework/commons/tuple/Tuple.java
@@ -1,10 +1,16 @@
 package com.wokoworks.framework.commons.tuple;
 
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
 /**
  * java 元组工具类
  * @author 0x0001
  */
 public class Tuple {
+
+    /* --------------------- boxed ----------------- */
 
     public static <A, B> TwoTuple<A, B> of(A first, B second) {
         return new TwoTuple<>(first, second);
@@ -18,32 +24,161 @@ public class Tuple {
         return new FourTuple<>(first, second, three, four);
     }
 
-    public static class TwoTuple<A, B> {
+    /* --------------------- int ----------------- */
 
+    public static IntTuple of(int first, int second) {
+        return new IntTuple(first, second);
+    }
+
+    public static IntThreeTuple of(int first, int second, int three) {
+        return new IntThreeTuple(first, second, three);
+    }
+
+    public static IntFourTuple of(int first, int second, int three, int four) {
+        return new IntFourTuple(first, second, three, four);
+    }
+
+    /* --------------------- long ----------------- */
+
+    public static LongTuple of(long first, long second) {
+        return new LongTuple(first, second);
+    }
+
+    public static LongThreeTuple of(long first, long second, long three) {
+        return new LongThreeTuple(first, second, three);
+    }
+
+    public static LongFourTuple of(long first, long second, long three, long four) {
+        return new LongFourTuple(first, second, three, four);
+    }
+
+    /* --------------------- double ----------------- */
+
+    public static DoubleTuple of(double first, double second) {
+        return new DoubleTuple(first, second);
+    }
+
+    public static DoubleThreeTuple of(double first, double second, double three) {
+        return new DoubleThreeTuple(first, second, three);
+    }
+
+    public static DoubleFourTuple of(double first, double second, double three, double four) {
+        return new DoubleFourTuple(first, second, three, four);
+    }
+
+    /* --------------------- int tuple ----------------- */
+
+    @ToString
+    @EqualsAndHashCode
+    @RequiredArgsConstructor
+    public static final class IntTuple {
+        public final int first;
+        public final int second;
+    }
+
+    @ToString
+    @EqualsAndHashCode
+    @RequiredArgsConstructor
+    public static final class IntThreeTuple {
+        public final int first;
+        public final int second;
+        public final int three;
+    }
+
+    @ToString
+    @EqualsAndHashCode
+    @RequiredArgsConstructor
+    public static final class IntFourTuple {
+        public final int first;
+        public final int second;
+        public final int three;
+        public final int four;
+    }
+
+    /* --------------------- long tuple ----------------- */
+
+    @ToString
+    @EqualsAndHashCode
+    @RequiredArgsConstructor
+    public static final class LongTuple {
+        public final long first;
+        public final long second;
+    }
+
+    @ToString
+    @EqualsAndHashCode
+    @RequiredArgsConstructor
+    public static final class LongThreeTuple {
+        public final long first;
+        public final long second;
+        public final long three;
+    }
+
+    @ToString
+    @EqualsAndHashCode
+    @RequiredArgsConstructor
+    public static final class LongFourTuple {
+        public final long first;
+        public final long second;
+        public final long three;
+        public final long four;
+    }
+
+    /* --------------------- double tuple ----------------- */
+
+    @ToString
+    @EqualsAndHashCode
+    @RequiredArgsConstructor
+    public static final class DoubleTuple {
+        public final double first;
+        public final double second;
+    }
+
+    @ToString
+    @EqualsAndHashCode
+    @RequiredArgsConstructor
+    public static final class DoubleThreeTuple {
+        public final double first;
+        public final double second;
+        public final double three;
+    }
+
+    @ToString
+    @EqualsAndHashCode
+    @RequiredArgsConstructor
+    public static final class DoubleFourTuple {
+        public final double first;
+        public final double second;
+        public final double three;
+        public final double four;
+    }
+
+    /* --------------------- Generic tuple ----------------- */
+
+    @ToString
+    @EqualsAndHashCode
+    @RequiredArgsConstructor
+    public static final class TwoTuple<A, B> {
         public final A first;
         public final B second;
-
-        TwoTuple(A first, B second) {
-            this.first = first;
-            this.second = second;
-        }
     }
 
-    public static class ThreeTuple<A, B, C> extends TwoTuple<A, B> {
+    @ToString
+    @EqualsAndHashCode
+    @RequiredArgsConstructor
+    public static final class ThreeTuple<A, B, C> {
+        public final A first;
+        public final B second;
         public final C three;
-
-        ThreeTuple(A first, B second, C three) {
-            super(first, second);
-            this.three = three;
-        }
     }
 
-    public static class FourTuple<A, B, C, D> extends ThreeTuple<A, B, C> {
+    @ToString
+    @EqualsAndHashCode
+    @RequiredArgsConstructor
+    public static final class FourTuple<A, B, C, D> {
+        public final A first;
+        public final B second;
+        public final C three;
         public final D four;
-
-        FourTuple(A first, B second, C three, D four) {
-            super(first, second, three);
-            this.four = four;
-        }
     }
 }
--- commons/src/test/java/com/wokoworks/framework/commons/tuple/TupleTest.java
+++ commons/src/test/java/com/wokoworks/framework/commons/tuple/TupleTest.java
@@ -3,8 +3,6 @@ package com.wokoworks.framework.commons.tuple;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class TupleTest {
 
     @Test
@@ -41,5 +39,118 @@ public class TupleTest {
         Assert.assertEquals("second equal", second, tuple.second);
         Assert.assertEquals("three equal", three, tuple.three);
         Assert.assertEquals("four equal", four, tuple.four);
+    }
+
+    /* int test */
+
+    @Test
+    public void ofInt() {
+        final int first = 1;
+        final int second = 2;
+        final Tuple.IntTuple tuple = Tuple.of(first, second);
+
+        Assert.assertEquals("first equal", first, tuple.first);
+        Assert.assertEquals("second equal", second, tuple.second);
+    }
+
+    @Test
+    public void ofInt1() {
+        final int first = 1;
+        final int second = 2;
+        final int three = 3;
+        final Tuple.IntThreeTuple tuple = Tuple.of(first, second, three);
+
+        Assert.assertEquals("first equal", first, tuple.first);
+        Assert.assertEquals("second equal", second, tuple.second);
+        Assert.assertEquals("three equal", three, tuple.three);
+    }
+
+    @Test
+    public void ofInt2() {
+        final int first = 1;
+        final int second = 2;
+        final int three = 3;
+        final int four = 4;
+        final Tuple.IntFourTuple tuple = Tuple.of(first, second, three, four);
+
+        Assert.assertEquals("first equal", first, tuple.first);
+        Assert.assertEquals("second equal", second, tuple.second);
+        Assert.assertEquals("three equal", three, tuple.three);
+        Assert.assertEquals("four equal", four, tuple.four);
+    }
+
+    /* long test */
+
+    @Test
+    public void ofLong() {
+        final long first = 1;
+        final long second = 2;
+        final Tuple.LongTuple tuple = Tuple.of(first, second);
+
+        Assert.assertEquals("first equal", first, tuple.first);
+        Assert.assertEquals("second equal", second, tuple.second);
+    }
+
+    @Test
+    public void ofLong1() {
+        final long first = 1;
+        final long second = 2;
+        final long three = 3;
+        final Tuple.LongThreeTuple tuple = Tuple.of(first, second, three);
+
+        Assert.assertEquals("first equal", first, tuple.first);
+        Assert.assertEquals("second equal", second, tuple.second);
+        Assert.assertEquals("three equal", three, tuple.three);
+    }
+
+    @Test
+    public void ofLong2() {
+        final long first = 1;
+        final long second = 2;
+        final long three = 3;
+        final long four = 4;
+        final Tuple.LongFourTuple tuple = Tuple.of(first, second, three, four);
+
+        Assert.assertEquals("first equal", first, tuple.first);
+        Assert.assertEquals("second equal", second, tuple.second);
+        Assert.assertEquals("three equal", three, tuple.three);
+        Assert.assertEquals("four equal", four, tuple.four);
+    }
+
+    /* double test */
+    @Test
+    public void ofDouble() {
+        final double first = 1.01;
+        final double second = 2.01;
+        final Tuple.DoubleTuple tuple = Tuple.of(first, second);
+
+        Assert.assertEquals("first equal", first, tuple.first, 0.01);
+        Assert.assertEquals("second equal", second, tuple.second, 0.01);
+    }
+
+    @Test
+    public void ofDouble1() {
+        final double first = 1.01;
+        final double second = 2.01;
+        final double three = 3.01;
+        final Tuple.DoubleThreeTuple tuple = Tuple.of(first, second, three);
+
+        Assert.assertEquals("first equal", first, tuple.first, 0.01);
+        Assert.assertEquals("second equal", second, tuple.second, 0.01);
+        Assert.assertEquals("three equal", three, tuple.three, 0.01);
+    }
+
+    @Test
+    public void ofDouble2() {
+        final double first = 1.01;
+        final double second = 2.01;
+        final double three = 3.01;
+        final double four = 4.01;
+        final Tuple.DoubleFourTuple tuple = Tuple.of(first, second, three, four);
+
+        Assert.assertEquals("first equal", first, tuple.first, 0.01);
+        Assert.assertEquals("second equal", second, tuple.second, 0.01);
+        Assert.assertEquals("three equal", three, tuple.three, 0.01);
+        Assert.assertEquals("four equal", four, tuple.four, 0.01);
     }
 }
