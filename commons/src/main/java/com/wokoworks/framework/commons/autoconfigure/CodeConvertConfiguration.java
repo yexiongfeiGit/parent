f50e757329dcedc6fa29b4e820acb7893499a951
0x0001 <78712900@qq.com>
2018-12-20 11:43:12 +0800

    add code convert

--- commons/src/main/java/com/wokoworks/framework/commons/autoconfigure/CodeConvertConfiguration.java
+++ commons/src/main/java/com/wokoworks/framework/commons/autoconfigure/CodeConvertConfiguration.java
@@ -2,6 +2,7 @@ package com.wokoworks.framework.commons.autoconfigure;
 
 import com.wokoworks.framework.commons.CodeConvertFactory;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -11,22 +12,26 @@ import org.springframework.context.annotation.Configuration;
  * @author 0x0001
  */
 @Configuration
-public class CodeConvertConfiguration implements BeanPostProcessor {
-
-    private final CodeConvertFactory factory = new CodeConvertFactory();
+public class CodeConvertConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
     public CodeConvertFactory codeConvertFactory() {
-        return factory;
+        return new CodeConvertFactory();
     }
 
-    @Override
-    @SuppressWarnings("unchecked")
-    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        if (bean instanceof CodeConvertFactory.CodeConvert) {
-            factory.register(((CodeConvertFactory.CodeConvert) bean));
+    @Configuration
+    public static class CodeConvertBeanPostProcessor implements BeanPostProcessor {
+        @Autowired
+        private CodeConvertFactory factory;
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+            if (bean instanceof CodeConvertFactory.CodeConvert) {
+                factory.register(((CodeConvertFactory.CodeConvert) bean));
+            }
+            return bean;
         }
-        return bean;
     }
 }
